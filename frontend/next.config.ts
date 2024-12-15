456ae9c3b9ddb7d890ebd7134644b19b1dce5dab
isheng <110703011@g.nccu.edu.tw>
2024-12-15 20:31:28 +0800

    pop: still there is a build problem inside the container, but is good if I pass out which I compiled beforehand, try to fix it later

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // Set the output to 'export' for static file generation
-  distDir: "out", // Custom output directory  //output: "standalone",
+  distDir: "out", // Custom output directory
+  //output: "standalone",
   env: {
     NEXT_PUBLIC_RECOMMEND_API_URL:
       process.env.RECOMMEND_API_URL || "http://localhost:5001",
