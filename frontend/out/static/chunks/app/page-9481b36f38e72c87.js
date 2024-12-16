6ae1cd8350d9873cdb7910aa51f299084691b80f
isheng <110703011@g.nccu.edu.tw>
2024-12-16 12:16:06 +0800

    fix: add out to deploy

---
+++ frontend/out/static/chunks/app/page-9481b36f38e72c87.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2538:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4839,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[839,441,517,358],()=>s(2538)),_N_E=e.O()}]);
